d9fa79feca6fc82bedf474555d7e2cd79dff5b4a
jescobarca <79493851+jescobarca@users.noreply.github.com>
2023-03-04 21:45:31 -0500

    Add files via upload

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author jorge
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        double suma=0,x=20,y=40;
+        suma=suma+x;
+        x=x+(y*y);
+        suma=suma+x/y;
+        System.out.println("EL VALOR DE LA SUMA:"+suma);
+    }
+}
